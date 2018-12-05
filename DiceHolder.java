19f0ca612a7187b421f6cedb01c821356d0bd967
AdamEGorka <45395187+AdamEGorka@users.noreply.github.com>
2018-12-05 09:06:22 -0500

    Default Constructor
    Declares blank ArrayList

--- DiceHolder.java
+++ DiceHolder.java
@@ -1,7 +1,10 @@
 import java.util.ArrayList;
 public class DiceHolder{
-	private ArrayList<Die> cup = new ArrayList<>();
+	private ArrayList<Die> cup;
 
+	public DiceHolder(){
+		cup = new ArrayList<>();
+	}
 	public int addDie(Die die){
 		if(cup.size() < 6)
 			cup.add(die);
