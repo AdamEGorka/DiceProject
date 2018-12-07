6ac1e17d2e1c9d54317734763903de50f50d5de7
Krissy <45279097+krissypins@users.noreply.github.com>
2018-12-07 09:37:33 -0500

    Test 1
    rolls dice until snake eyes are rolled

--- DiceRunner.java
+++ DiceRunner.java
@@ -1,7 +1,16 @@
 public class DiceRunner{
 	public static void main (String[]args){
 		Die dice = new Die();
+		Die dice2 = new Die();
+		boolean snakeEyes = false;
+
+		while(!snakeEyes){
 		dice.roll();
-		System.out.println(dice.toString());
+		dice2.roll();
+		System.out.println(dice.toString() + "\t" + dice2.toString());
+		if((dice.getValue() == 1) && (dice2.getValue() == 1))
+			snakeEyes = true;
+		}
+
 	}
 }
