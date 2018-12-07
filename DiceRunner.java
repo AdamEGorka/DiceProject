c34ac3b1a58b61626a5936c865a3e2e714c635be
Krissy <45279097+krissypins@users.noreply.github.com>
2018-12-07 09:44:20 -0500

    added a counter for # of rolls
    makes it easier to see how many times the dice were rolled before snake eyes were rolled

--- DiceRunner.java
+++ DiceRunner.java
@@ -3,10 +3,13 @@ public class DiceRunner{
 		Die dice = new Die();
 		Die dice2 = new Die();
 		boolean snakeEyes = false;
+		int roll = 0;
 
 		while(!snakeEyes){
+		roll++;
 		dice.roll();
 		dice2.roll();
+		System.out.println("Roll " + roll + ":");
 		System.out.println(dice.toString() + "\t" + dice2.toString());
 		if((dice.getValue() == 1) && (dice2.getValue() == 1))
 			snakeEyes = true;
